bcc4eb54df99dff6f891183958df67d17e14f173
Luciano Júnior <42066765+lamjunioor@users.noreply.github.com>
2025-03-28 13:04:05 -0300

    Update app.py

--- view/app.py
+++ view/app.py
@@ -2,5 +2,6 @@ import dash
 import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1'},],)
+server = app.server
 app.config.suppress_callback_exceptions = True
 app.title = 'CORLI Results'
